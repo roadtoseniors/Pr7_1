395f597e00c1e744df9a10c738aeda3eeeb02e4a
kekmaster <kekmaster@DESKTOP-AB4C90S>
2026-03-26 12:55:22 +0300

    сделали внутрянку wpf

--- Encoder_Decoder/MainWindow.xaml.cs
+++ Encoder_Decoder/MainWindow.xaml.cs
@@ -20,9 +20,58 @@ namespace Encoder_Decoder
     /// </summary>
     public partial class MainWindow : Window
     {
+        private HillCipher cipher = new HillCipher();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private int[,] ParseMatrix(string input)
+        {
+            try
+            {
+                var rows = input.Split(';');
+                int[,] matrix = new int[2, 2];
+
+                for (int i = 0; i < 2; i++)
+                {
+                    var cols = rows[i].Trim().Split(' ');
+                    for (int j = 0; j < 2; j++)
+                        matrix[i, j] = int.Parse(cols[j]);
+                }
+
+                return matrix;
+            }
+            catch
+            {
+                throw new Exception("Неверный формат матрицы");
+            }
+        }
+
+        private void Encrypt_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var matrix = ParseMatrix(MatrixBox.Text);
+                ResultTextBox.Text = cipher.Encrypt(InputTextBox.Text, matrix);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Decrypt_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var matrix = ParseMatrix(MatrixBox.Text);
+                ResultTextBox.Text = cipher.Decrypt(InputTextBox.Text, matrix);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
